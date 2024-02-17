8775e3aedd65195f1c9acb011e4a3a58cfa4bdcf
Zach <zpalmer618@gmail.com>
2024-02-17 10:13:06 -0600

    added a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use donkey::{colors::GRAY, Window};
+
 fn main() {
-    println!("Hello, world!");
+    let w = 800;
+    let h = 600;
+    let win = Window::init(w, h, "Window for fun");
+    win.set_target_fps(60);
+    let color = GRAY;
+    while !win.should_close() {
+        win.begin_drawing();
+        win.clear_background(color);
+        win.end_drawing()
+    }
 }
