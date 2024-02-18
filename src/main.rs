be6995f8f847ec17794725652d499db164021b6f
Zach <zpalmer618@gmail.com>
2024-02-18 10:24:38 -0600

    replaced manual with automatic rectangles

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use donkey::{
-    colors::{BLACK, BLUE, GRAY, GREEN, ORANGE, RED, WHITE},
+    colors::{BLACK, GRAY},
     keys::Key,
     Window,
 };
+use raylib_sys::{
+    DrawRectangleRec, GetScreenHeight, IsKeyDown, KeyboardKey_KEY_LEFT_SHIFT, Rectangle,
+};
 
+// Define Screen Management
 enum GameScreen {
     Logo,
     Title,
@@ -12,84 +16,100 @@ enum GameScreen {
 }
 
 fn main() {
-    let w = 800;
-    let h = 600;
-    let title = "Game Window";
-    let win = Window::init(w, h, title);
-    win.set_target_fps(60);
-    let mut rectpos = donkey::vector2!((w / 2) as f32, (h / 2) as f32);
-    let rectsize = donkey::vector2!(200., 100.);
-    let mut currentscreen = GameScreen::Logo;
-    while !win.should_close() {
-        win.begin_drawing();
-        match currentscreen {
-            GameScreen::Logo => {
-                win.clear_background(GRAY);
-                win.draw_text(
-                    "Here is where the logo for the game will go",
-                    100,
-                    100,
-                    25,
-                    BLACK,
-                );
-                if win.is_key_pressed(Key::Enter) {
-                    currentscreen = GameScreen::Title
+    unsafe {
+        let w = 1280;
+        let h = 720;
+        let title = "Game Window";
+        let win = Window::init(w, h, title);
+        win.set_target_fps(60);
+        let mut box_a = Rectangle {
+            x: 10.,
+            y: (GetScreenHeight() as f32 / 2. - 50.) as f32,
+            width: 200.,
+            height: 100.,
+        };
+        let mut currentscreen = GameScreen::Logo;
+        while !win.should_close() {
+            win.begin_drawing();
+            match currentscreen {
+                GameScreen::Logo => {
+                    win.clear_background(GRAY);
+                    win.draw_text(
+                        "Here is where the logo for the game will go",
+                        100,
+                        100,
+                        25,
+                        BLACK,
+                    );
+                    if win.is_key_pressed(Key::Enter) {
+                        currentscreen = GameScreen::Title
+                    }
+                }
+                GameScreen::Title => {
+                    win.clear_background(GRAY);
+                    win.draw_text(
+                        "Here is where the title screen will go",
+                        100,
+                        100,
+                        25,
+                        BLACK,
+                    );
+                    if win.is_key_pressed(Key::Enter) {
+                        currentscreen = GameScreen::Gameplay
+                    }
+                }
+                GameScreen::Gameplay => {
+                    let fac = if IsKeyDown(KeyboardKey_KEY_LEFT_SHIFT as i32) {
+                        2.0
+                    } else {
+                        1.0
+                    };
+                    if win.is_key_down(Key::W) {
+                        box_a.y -= 2.0 * fac;
+                    }
+                    if win.is_key_down(Key::A) {
+                        box_a.x -= 2.0 * fac;
+                    }
+                    if win.is_key_down(Key::S) {
+                        box_a.y += 2.0 * fac;
+                    }
+                    if win.is_key_down(Key::D) {
+                        box_a.x += 2.0 * fac;
+                    }
+                    win.clear_background(GRAY);
+                    win.draw_text("Move your rectangle with WASD", 100, 100, 25, BLACK);
+                    DrawRectangleRec(box_a, BLACK);
+                    if win.is_key_pressed(Key::Enter) {
+                        currentscreen = GameScreen::Ending
+                    }
+                }
+                GameScreen::Ending => {
+                    win.clear_background(GRAY);
+                    win.draw_text(
+                        "Here is where the ending screen will go",
+                        100,
+                        100,
+                        25,
+                        BLACK,
+                    );
+                    if win.is_key_pressed(Key::Enter) {
+                        currentscreen = GameScreen::Title
+                    }
                 }
             }
-            GameScreen::Title => {
-                win.clear_background(GRAY);
-                win.draw_text(
-                    "Here is where the title screen will go",
-                    100,
-                    100,
-                    25,
-                    BLACK,
-                );
-                if win.is_key_pressed(Key::Enter) {
-                    currentscreen = GameScreen::Gameplay
-                }
-            }
-            GameScreen::Gameplay => {
-                if win.is_key_down(Key::W) {
-                    rectpos.y -= 2.0;
-                }
-                if win.is_key_down(Key::A) {
-                    rectpos.x -= 2.0;
-                }
-                if win.is_key_down(Key::S) {
-                    rectpos.y += 2.0;
-                }
-                if win.is_key_down(Key::D) {
-                    rectpos.x += 2.0;
-                }
-                win.clear_background(GRAY);
-                win.draw_text("Move your rectangle with WASD", 100, 100, 25, WHITE);
-                win.draw_rectangle_v(rectpos, rectsize, BLACK);
-                win.draw_text(
-                    "Player 1",
-                    rectpos.x as usize ,
-                    rectpos.y as usize,
-                    20,
-                    WHITE,
-                );
-                if win.is_key_pressed(Key::Enter) {
-                    currentscreen = GameScreen::Ending
-                }
-            }
-            GameScreen::Ending => {
-                win.clear_background(GRAY);
-                win.draw_text(
-                    "Here is where the ending screen will go",
-                    100,
-                    100,
-                    25,
-                    BLACK,
-                );
-                if win.is_key_pressed(Key::Enter) {
-                    currentscreen = GameScreen::Title
-                }
-            }
+            win.end_drawing()
         }
-        win.end_drawing()
     }
 }
+
+// The below is another way of manual making a movable rectangle.
+// let mut rectpos = donkey::vector2!((w / 2) as f32, (h / 2) as f32);
+// let rectsize = donkey::vector2!(200., 100.);
+// win.draw_rectangle_v(rectpos, rectsize, BLACK);
+// win.draw_text(
+//     "Player 1",
+//     (rectpos.x + win.measure_text("Player 1", 20) as f32 * 0.7) as usize,
+//     rectpos.y as usize,
+//     20,
+//     WHITE,
+// );
