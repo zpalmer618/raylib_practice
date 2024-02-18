eb209dee852878a1c452587a4db6b9ae02ca0a8b
Zach <zpalmer618@gmail.com>
2024-02-18 08:40:10 -0600

    removed crazy colors for grey and black scheme

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     let win = Window::init(w, h, title);
     win.set_target_fps(60);
     let mut rectpos = donkey::vector2!((w / 2) as f32, (h / 2) as f32);
-    let rectsize = donkey::vector2!(200.,100.);
+    let rectsize = donkey::vector2!(200., 100.);
     let mut currentscreen = GameScreen::Logo;
     while !win.should_close() {
         win.begin_drawing();
@@ -37,13 +37,13 @@ fn main() {
                 }
             }
             GameScreen::Title => {
-                win.clear_background(RED);
+                win.clear_background(GRAY);
                 win.draw_text(
                     "Here is where the title screen will go",
                     100,
                     100,
                     25,
-                    WHITE,
+                    BLACK,
                 );
                 if win.is_key_pressed(Key::Enter) {
                     currentscreen = GameScreen::Gameplay
@@ -62,22 +62,28 @@ fn main() {
                 if win.is_key_down(Key::D) {
                     rectpos.x += 2.0;
                 }
-                win.clear_background(BLACK);
-                win.draw_text("Move your rectangle with WASD", 100, 100, 25, ORANGE);
-                win.draw_rectangle_v(rectpos, rectsize, WHITE);
-                win.draw_text("Player 1", rectpos.x as usize, rectpos.y as usize, 20, BLACK);
+                win.clear_background(GRAY);
+                win.draw_text("Move your rectangle with WASD", 100, 100, 25, WHITE);
+                win.draw_rectangle_v(rectpos, rectsize, BLACK);
+                win.draw_text(
+                    "Player 1",
+                    rectpos.x as usize ,
+                    rectpos.y as usize,
+                    20,
+                    WHITE,
+                );
                 if win.is_key_pressed(Key::Enter) {
                     currentscreen = GameScreen::Ending
                 }
             }
             GameScreen::Ending => {
-                win.clear_background(BLUE);
+                win.clear_background(GRAY);
                 win.draw_text(
                     "Here is where the ending screen will go",
                     100,
                     100,
                     25,
-                    GREEN,
+                    BLACK,
                 );
                 if win.is_key_pressed(Key::Enter) {
                     currentscreen = GameScreen::Title
