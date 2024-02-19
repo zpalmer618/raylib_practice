ba96aeccf31a84f199a81d09294580333af32c66
Zach <zpalmer618@gmail.com>
2024-02-19 06:53:07 -0600

    switched all raylib-sys to donkey bindings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use donkey::{
     colors::{BLACK, GRAY},
     keys::Key,
-    Window,
-};
-use raylib_sys::{
-    DrawRectangleRec, GetScreenHeight, IsKeyDown, KeyboardKey_KEY_LEFT_SHIFT, Rectangle,
+    Rectangle, Window,
 };
 
 // Define Screen Management
@@ -16,89 +13,87 @@ enum GameScreen {
 }
 
 fn main() {
-    unsafe {
-        let w = 1280;
-        let h = 720;
-        let title = "Game Window";
-        let win = Window::init(w, h, title);
-        win.set_target_fps(60);
-        let mut box_a = Rectangle {
-            x: 10.,
-            y: (GetScreenHeight() as f32 / 2. - 50.) as f32,
-            width: 200.,
-            height: 100.,
-        };
-        let mut currentscreen = GameScreen::Logo;
-        while !win.should_close() {
-            win.begin_drawing();
-            match currentscreen {
-                GameScreen::Logo => {
-                    win.clear_background(GRAY);
-                    win.draw_text(
-                        "Here is where the logo for the game will go",
-                        100,
-                        100,
-                        25,
-                        BLACK,
-                    );
-                    if win.is_key_pressed(Key::Enter) {
-                        currentscreen = GameScreen::Title
-                    }
-                }
-                GameScreen::Title => {
-                    win.clear_background(GRAY);
-                    win.draw_text(
-                        "Here is where the title screen will go",
-                        100,
-                        100,
-                        25,
-                        BLACK,
-                    );
-                    if win.is_key_pressed(Key::Enter) {
-                        currentscreen = GameScreen::Gameplay
-                    }
-                }
-                GameScreen::Gameplay => {
-                    let fac = if IsKeyDown(KeyboardKey_KEY_LEFT_SHIFT as i32) {
-                        2.0
-                    } else {
-                        1.0
-                    };
-                    if win.is_key_down(Key::W) {
-                        box_a.y -= 2.0 * fac;
-                    }
-                    if win.is_key_down(Key::A) {
-                        box_a.x -= 2.0 * fac;
-                    }
-                    if win.is_key_down(Key::S) {
-                        box_a.y += 2.0 * fac;
-                    }
-                    if win.is_key_down(Key::D) {
-                        box_a.x += 2.0 * fac;
-                    }
-                    win.clear_background(GRAY);
-                    win.draw_text("Move your rectangle with WASD", 100, 100, 25, BLACK);
-                    DrawRectangleRec(box_a, BLACK);
-                    if win.is_key_pressed(Key::Enter) {
-                        currentscreen = GameScreen::Ending
-                    }
-                }
-                GameScreen::Ending => {
-                    win.clear_background(GRAY);
-                    win.draw_text(
-                        "Here is where the ending screen will go",
-                        100,
-                        100,
-                        25,
-                        BLACK,
-                    );
-                    if win.is_key_pressed(Key::Enter) {
-                        currentscreen = GameScreen::Title
-                    }
+    let w = 1280;
+    let h = 720;
+    let title = "Game Window";
+    let win = Window::init(w, h, title);
+    win.set_target_fps(60);
+    let mut box_a = Rectangle {
+        x: 10.,
+        y: (win.get_screen_height() as f32 / 2. - 50.) as f32,
+        width: w as f32 / 3.2,
+        height: h as f32 / 3.6,
+    };
+    let mut currentscreen = GameScreen::Logo;
+    while !win.should_close() {
+        win.begin_drawing();
+        match currentscreen {
+            GameScreen::Logo => {
+                win.clear_background(GRAY);
+                win.draw_text(
+                    "Here is where the logo for the game will go",
+                    100,
+                    100,
+                    25,
+                    BLACK,
+                );
+                if win.is_key_pressed(Key::Enter) {
+                    currentscreen = GameScreen::Title
                 }
             }
-            win.end_drawing()
+            GameScreen::Title => {
+                win.clear_background(GRAY);
+                win.draw_text(
+                    "Here is where the title screen will go",
+                    100,
+                    100,
+                    25,
+                    BLACK,
+                );
+                if win.is_key_pressed(Key::Enter) {
+                    currentscreen = GameScreen::Gameplay
+                }
+            }
+            GameScreen::Gameplay => {
+                let fac = if win.is_key_down(Key::LShift) {
+                    2.0
+                } else {
+                    1.0
+                };
+                if win.is_key_down(Key::W) {
+                    box_a.y -= 2.0 * fac;
+                }
+                if win.is_key_down(Key::A) {
+                    box_a.x -= 2.0 * fac;
+                }
+                if win.is_key_down(Key::S) {
+                    box_a.y += 2.0 * fac;
+                }
+                if win.is_key_down(Key::D) {
+                    box_a.x += 2.0 * fac;
+                }
+                win.clear_background(GRAY);
+                win.draw_text("Move your rectangle with WASD", 100, 100, 25, BLACK);
+                win.draw_rectangle_rec(box_a, BLACK);
+                if win.is_key_pressed(Key::Enter) {
+                    currentscreen = GameScreen::Ending
+                }
+            }
+            GameScreen::Ending => {
+                win.clear_background(GRAY);
+                win.draw_text(
+                    "Here is where the ending screen will go",
+                    100,
+                    100,
+                    25,
+                    BLACK,
+                );
+                if win.is_key_pressed(Key::Enter) {
+                    currentscreen = GameScreen::Title
+                }
+            }
         }
+        win.end_drawing()
     }
 }
 
